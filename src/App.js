01a070ded0a88fb221e5226a9abe3c90c63f3bd3
pyr0gan <pyr0gan@me.com>
2021-03-25 03:30:35 +0000

    add antd + examples

--- src/App.js
+++ src/App.js
@@ -1,203 +1,186 @@
-import 'regenerator-runtime/runtime'
-import React from 'react'
-import { login, logout } from './utils'
-import './global.css'
+import 'regenerator-runtime/runtime';
+import React, { useState } from 'react';
+import { login, logout, } from './utils';
+import './global.css';
+import logowhite from './assets/logo-white.svg';
+import { Layout, Button, Typography, Avatar } from 'antd';
+import { HighlightOutlined, SmileOutlined, SmileFilled, UserOutlined } from '@ant-design/icons';
 
-import getConfig from './config'
-const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+import getConfig from './config';
+const { networkId } = getConfig(process.env.NODE_ENV || 'development');
+const { Paragraph } = Typography;
+const { Header, Footer, Content } = Layout;
 
 export default function App() {
-  // use React Hooks to store greeting in component state
-  const [greeting, setGreeting] = React.useState()
+    // use React Hooks to store greeting in component state
+    const [greeting, setGreeting] = React.useState();
 
-  // when the user has not yet interacted with the form, disable the button
-  const [buttonDisabled, setButtonDisabled] = React.useState(true)
+    // when the user has not yet interacted with the form, disable the button
+    const [buttonDisabled, setButtonDisabled] = React.useState(true);
 
-  // after submitting the form, we want to show Notification
-  const [showNotification, setShowNotification] = React.useState(false)
+    // after submitting the form, we want to show Notification
+    const [showNotification, setShowNotification] = React.useState(false);
 
-  // The useEffect hook can be used to fire side-effects during render
-  // Learn more: https://reactjs.org/docs/hooks-intro.html
-  React.useEffect(
-    () => {
-      // in this case, we only care to query the contract when signed in
-      if (window.walletConnection.isSignedIn()) {
+    // The useEffect hook can be used to fire side-effects during render
+    // Learn more: https://reactjs.org/docs/hooks-intro.html
+    React.useEffect(
+        () => {
+            // in this case, we only care to query the contract when signed in
+            if (window.walletConnection.isSignedIn()) {
 
-        // window.contract is set by initContract in index.js
-        window.contract.getGreeting({ accountId: window.accountId })
-          .then(greetingFromContract => {
-            setGreeting(greetingFromContract)
-          })
-      }
-    },
+                // window.contract is set by initContract in index.js
+                window.contract.getGreeting({ accountId: window.accountId })
+                    .then(greetingFromContract => {
+                        setGreeting(greetingFromContract);
+                    });
+            }
+        },
 
-    // The second argument to useEffect tells React when to re-run the effect
-    // Use an empty array to specify "only run on first render"
-    // This works because signing into NEAR Wallet reloads the page
-    []
-  )
+        // The second argument to useEffect tells React when to re-run the effect
+        // Use an empty array to specify "only run on first render"
+        // This works because signing into NEAR Wallet reloads the page
+        []
+    );
 
-  // if not signed in, return early with sign-in prompt
-  if (!window.walletConnection.isSignedIn()) {
+    // if not signed in, return early with sign-in prompt
+    if (!window.walletConnection.isSignedIn()) {
+        return (
+            <main>
+                <h2>Welcome to the Figment Learn Intermediate NEAR Pathway!</h2>
+                <p>
+                    By default,<br /> this create-near-app project runs in development mode.<br />
+                    It connects to <b><a href='https://wallet.testnet.near.org' alt='wallet.testnet.near.org'>wallet.testnet.near.org</a></b>.
+                    This works just like the main network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
+                    convertible to other currencies – they're just for testing!
+                </p>
+                <p align='center' style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
+                    To make use of this app running on the NEAR blockchain, you need to sign in!<br /> You can create a <a href="https://docs.near.org/docs/develop/basics/create-account">NEAR testnet account</a>, if you do not already have one.
+                </p>
+                <p style={{ textAlign: 'center', marginTop: '1.5em', marginBottom: '1.5em', }}>
+                    <button onClick={login}>Sign in</button>
+                </p>
+                <p style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
+                    It is important to note that if you use this project from a gitpod, you will not need to configure your local development environment in any way to make use of this create-near-app based template.<br />
+                    You are, of course, free to clone the repository and work on it locally - however this may require you <br />to perform additional configuration on your system.<br /><br />
+                    The documentation on the GitHub repository should be sufficient to get you going, and there are additional links to offsite documentation there as well. Enjoy, and happy hacking!<br /><br />
+                     - the Learn Team @ <a href="https://figment.io">Figment</a>
+                </p>
+            </main>
+        );
+    }
+
     return (
-    <main>
-    <h2>Welcome to the Figment Learn Intermediate NEAR Pathway!</h2>
-    <p>
-      By default,<br/> this create-near-app project runs in development mode.<br/>
-      It connects to <b><a href='https://wallet.testnet.near.org' alt='wallet.testnet.near.org'>wallet.testnet.near.org</a></b>.
-      This works just like the main network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-      convertible to other currencies – they're just for testing!
-    </p>
-    <p align='center' style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
-      To make use of this app running on the NEAR blockchain, you need to sign in!<br/> You can create a <a href="https://docs.near.org/docs/develop/basics/create-account">NEAR testnet account</a>, if you do not already have one. 
-    </p>
-    <p style={{ textAlign: 'center', marginTop: '1.5em', marginBottom: '1.5em', }}>
-      <button onClick={login}>Sign in</button>
-    </p>
-    <p style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
-        It is important to note that if you use this project from a gitpod, you will not need to configure your local development environment in any way to make use of this create-near-app based template.<br/>
-        You are, of course, free to clone the repository and work on it locally - however this may require you <br/>to perform additional configuration on your system.<br/><br/>
-        The documentation on the GitHub repository should be sufficient to get you going, and there are additional links to offsite documentation there as well. Enjoy, and happy hacking!<br/><br/>
-        - the Learn Team @ <a href="https://figment.io">Figment</a>
-    </p>
-    </main>
-    )
-  }
+        // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
+        <>
+            <Layout>
+                <Header>
+                    <p style={{ fontSize: 'medium', verticalAlign: 'middle' }}> <Avatar icon={<UserOutlined />} /> <label htmlFor="greeting" style={{ color: 'var(--secondary)', borderBottom: '0px solid var(--secondary)' }}>{greeting}</label> {window.accountId}
+                        <Button type='primary' style={{ float: 'right', borderStyle: 'outset', borderWidth: '2px', fontSize: 'medium' }} onClick={logout}>
+                            🚀<img src={logowhite} width='32px' style={{ cursor: 'crosshair', verticalAlign: 'middle' }} /> Sign out
+                    </Button></p>
+                </Header>
+                <Layout>
+                    <Content>
+                        <main>
 
-  return (
-    // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
-    <>
-      <button className="link" style={{ float: 'right' }} onClick={logout}>
-        Sign out
-      </button>
-      <main>
-        <h1>
-          <label
-            htmlFor="greeting"
-            style={{
-              color: 'var(--secondary)',
-              borderBottom: '2px solid var(--secondary)'
-            }}
-          >
-            {greeting}
-          </label>
-          {' '/* React trims whitespace around tags; insert literal space character when needed */}
-          {window.accountId}!
-        </h1>
-        <form onSubmit={async event => {
-          event.preventDefault()
+                            <label htmlFor="greeting" style={{ color: 'var(--secondary)', borderBottom: '0px solid var(--secondary)' }}>
+                                {greeting}
+                            </label>
+                            {' '/* React trims whitespace around tags; insert literal space character when needed */}
+                            {window.accountId}!
 
-          // get elements from the form using their id attribute
-          const { fieldset, greeting } = event.target.elements
+                            <form onSubmit={async event => {
+                                event.preventDefault();
 
-          // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
-          const newGreeting = greeting.value
+                                // get elements from the form using their id attribute
+                                const { fieldset, greeting } = event.target.elements;
 
-          // disable the form while the value gets updated on-chain
-          fieldset.disabled = true
+                                // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
+                                const newGreeting = greeting.value;
 
-          try {
-            // make an update call to the smart contract
-            await window.contract.setGreeting({
-              // pass the value that the user entered in the greeting field
-              message: newGreeting
-            })
-          } catch (e) {
-            alert(
-              'Something went wrong! ' +
-              'Maybe you need to sign out and back in? ' +
-              'Check your browser console for more info.'
-            )
-            throw e
-          } finally {
-            // re-enable the form, whether the call succeeded or failed
-            fieldset.disabled = false
-          }
+                                // disable the form while the value gets updated on-chain
+                                fieldset.disabled = true;
 
-          // update local `greeting` variable to match persisted value
-          setGreeting(newGreeting)
+                                try {
+                                    // make an update call to the smart contract
+                                    await window.contract.setGreeting({
+                                        // pass the value that the user entered in the greeting field
+                                        message: newGreeting
+                                    });
+                                } catch (e) {
+                                    alert(
+                                        'Something went wrong! ' +
+                                        'Maybe you need to sign out and back in? ' +
+                                        'Check your browser console for more info.'
+                                    );
+                                    throw e;
+                                } finally {
+                                    // re-enable the form, whether the call succeeded or failed
+                                    fieldset.disabled = false;
+                                }
 
-          // show Notification
-          setShowNotification(true)
+                                // update local `greeting` variable to match persisted value
+                                setGreeting(newGreeting);
 
-          // remove Notification again after css animation completes
-          // this allows it to be shown again next time the form is submitted
-          setTimeout(() => {
-            setShowNotification(false)
-          }, 11000)
-        }}>
-          <fieldset id="fieldset">
-            <label
-              htmlFor="greeting"
-              style={{
-                display: 'block',
-                color: 'var(--gray)',
-                marginBottom: '0.5em'
-              }}
-            >
-              Change greeting
-            </label>
-            <div style={{ display: 'flex' }}>
-              <input
-                autoComplete="off"
-                defaultValue={greeting}
-                id="greeting"
-                onChange={e => setButtonDisabled(e.target.value === greeting)}
-                style={{ flex: 1 }}
-              />
-              <button
-                disabled={buttonDisabled}
-                style={{ borderRadius: '0 5px 5px 0' }}
-              >
-                Save
-              </button>
-            </div>
-          </fieldset>
-        </form>
+                                // show Notification
+                                setShowNotification(true);
 
-        <p>
-          Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
-        </p>
-        <ol>
-          <li>
-            Look in <code>src/App.js</code> and <code>src/utils.js</code> – you'll see <code>getGreeting</code> and <code>setGreeting</code> being called on <code>contract</code>. What's this?
-          </li>
-          <li>
-            Ultimately, this <code>contract</code> code is defined in <code>assembly/main.ts</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/roles/developer/contracts/intro">smart contract</a>.</li>
-          <li>
-            When you run <code>yarn dev</code>, the code in <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code> at the <code>scripts</code> section to find the <code>dev</code> command.</li>
-        </ol>
-        <hr />
-        <p>
-          To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
-        </p>
+                                // remove Notification again after css animation completes
+                                // this allows it to be shown again next time the form is submitted
+                                setTimeout(() => {
+                                    setShowNotification(false);
+                                }, 11000);
+                            }}>
+                                <fieldset id="fieldset">
+                                    <label htmlFor="greeting" style={{ display: 'block', color: 'var(--gray)', marginBottom: '0.5em' }}>
+                                        Change greeting
+                   </label>
+                                    <div style={{ display: 'flex' }}>
+                                        <input autoComplete="off" defaultValue={greeting} id="greeting" onChange={e => setButtonDisabled(e.target.value === greeting)} style={{ flex: 1 }} />
+                                        <button disabled={buttonDisabled} style={{ borderRadius: '0 5px 5px 0' }}>
+                                            Save
+                    </button>
+                                    </div>
+                                </fieldset>
+                            </form>
 
-      </main>
+                        </main>
 
-      {showNotification && <Notification />}
-      
-    </>
-  )
+                        {showNotification && <Notification />}
+
+                    </Content>
+
+                </Layout>
+
+                <Footer>
+                    <hr />
+                    <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/roles/developer/contracts/intro">smart contract</a>
+                </Footer>
+
+            </Layout>
+
+        </>
+    );
 }
 
 // this component gets rendered by App after the form is submitted
 function Notification() {
-  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
-  return (
-    <aside>
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
-        {window.accountId}
-      </a>
-      {' '/* React trims whitespace around tags; insert literal space character when needed */}
+    const urlPrefix = `https://explorer.${networkId}.near.org/accounts`;
+    return (
+        <aside>
+            <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
+                {window.accountId}
+            </a>
+            {' '/* React trims whitespace around tags; insert literal space character when needed */}
       called method: 'setGreeting' in contract:
-      {' '}
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
-        {window.contract.contractId}
-      </a>
-      <footer>
-        <div>✔ Succeeded</div>
-        <div>Just now</div>
-      </footer>
-    </aside>
-  )
+            {' '}
+            <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
+                {window.contract.contractId}
+            </a>
+            <footer>
+                <div>✔ Succeeded</div>
+                <div>Just now</div>
+            </footer>
+        </aside>
+    );
 }
