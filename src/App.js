5e072596b82155f60834ad8c2ad261484d99fd81
Rogan X <pyr0gan@me.com>
2021-04-01 11:01:58 -0600

    Pyr0gan/gitpod setup (#2)
    * add antd + examples
    
    * adding components
    
    * Update .gitpod.yml
    
    * Delete dist directory

--- src/App.js
+++ src/App.js
@@ -1,203 +1,166 @@
-import 'regenerator-runtime/runtime'
-import React from 'react'
-import { login, logout } from './utils'
-import './global.css'
+import 'regenerator-runtime/runtime';
+import React, { useState, useContext, useEffect } from 'react';
+import { appStore, onAppMount } from './state/app';
+import { Wallet } from './components/Wallet';
+import { Contract } from './components/Contract';
+import { Keys } from './components/Keys';
+import { Gallery } from './components/Gallery';
+import { login, logout, } from './utils';
+import './global.css';
+import logowhite from './assets/logo-white.svg';
+import { Layout, Button, Typography, Avatar, Carousel, Tooltip, Popover } from 'antd';
+import 'antd/dist/antd.dark.css';
+import { HighlightOutlined, SmileOutlined, SmileFilled, UserOutlined } from '@ant-design/icons';
 
-import getConfig from './config'
-const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+import getConfig from './config';
+import { configConsumerProps } from 'antd/lib/config-provider';
+
+const content = (
+    <div>
+        <p> <Gallery /> </p>
+    </div>
+);
+
+const contentStyle = {
+    height: '200px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
+
+const { networkId } = getConfig(process.env.NODE_ENV || 'development');
+const { Paragraph } = Typography;
+const { Header, Footer, Content } = Layout;
 
 export default function App() {
-  // use React Hooks to store greeting in component state
-  const [greeting, setGreeting] = React.useState()
 
-  // when the user has not yet interacted with the form, disable the button
-  const [buttonDisabled, setButtonDisabled] = React.useState(true)
+    // use React Hooks to store greeting in component state
+    const [greeting, setGreeting] = React.useState();
 
-  // after submitting the form, we want to show Notification
-  const [showNotification, setShowNotification] = React.useState(false)
+    // when the user has not yet interacted with the form, disable the button
+    const [buttonDisabled, setButtonDisabled] = React.useState(true);
 
-  // The useEffect hook can be used to fire side-effects during render
-  // Learn more: https://reactjs.org/docs/hooks-intro.html
-  React.useEffect(
-    () => {
-      // in this case, we only care to query the contract when signed in
-      if (window.walletConnection.isSignedIn()) {
+    // after submitting the form, we want to show Notification
+    const [showNotification, setShowNotification] = React.useState(false);
 
-        // window.contract is set by initContract in index.js
-        window.contract.getGreeting({ accountId: window.accountId })
-          .then(greetingFromContract => {
-            setGreeting(greetingFromContract)
-          })
-      }
-    },
+    // The useEffect hook can be used to fire side-effects during render
+    // Learn more: https://reactjs.org/docs/hooks-intro.html
+    React.useEffect(
+        () => {
+            // in this case, we only care to query the contract when signed in
+            if (window.walletConnection.isSignedIn()) {
 
-    // The second argument to useEffect tells React when to re-run the effect
-    // Use an empty array to specify "only run on first render"
-    // This works because signing into NEAR Wallet reloads the page
-    []
-  )
+                // window.contract is set by initContract in index.js
+                window.contract.getGreeting({ accountId: window.accountId })
+                    .then(greetingFromContract => {
+                        setGreeting(greetingFromContract);
+                    });
+            }
+        },
 
-  // if not signed in, return early with sign-in prompt
-  if (!window.walletConnection.isSignedIn()) {
+        // The second argument to useEffect tells React when to re-run the effect
+        // Use an empty array to specify "only run on first render"
+        // This works because signing into NEAR Wallet reloads the page
+        []
+    );
+
+    // if not signed in, return early with sign-in prompt
+    if (!window.walletConnection.isSignedIn()) {
+        return (
+            <main>
+                <h2>Welcome to the Figment Learn Intermediate NEAR Pathway!</h2>
+                <p>
+                    By default,<br /> this create-near-app project runs in development mode.<br />
+                    It connects to <b><a href='https://wallet.testnet.near.org' alt='wallet.testnet.near.org'>wallet.testnet.near.org</a></b>.
+                    This works just like the main network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
+                    convertible to other currencies – they're just for testing!
+                </p>
+                <p align='center' style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
+                    To make use of this app running on the NEAR blockchain, you need to sign in!<br /> You can create a <a href="https://docs.near.org/docs/develop/basics/create-account">NEAR testnet account</a>, if you do not already have one.
+                </p>
+                <p style={{ textAlign: 'center', marginTop: '1.5em', marginBottom: '1.5em', }}>
+                    <button onClick={login}>Sign in</button>
+                </p>
+                <p style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
+                    It is important to note that if you use this project from a gitpod, you will not need to configure your local development environment in any way to make use of this create-near-app based template.<br />
+                    You are, of course, free to clone the repository and work on it locally - however this may require you <br />to perform additional configuration on your system.<br /><br />
+                    The documentation on the GitHub repository should be sufficient to get you going, and there are additional links to offsite documentation there as well. Enjoy, and happy hacking!<br /><br />
+                     - the Learn Team @ <a href="https://figment.io">Figment</a>
+                </p>
+            </main>
+        );
+    }
+
     return (
-    <main>
-    <h2>Welcome to the Figment Learn Intermediate NEAR Pathway!</h2>
-    <p>
-      By default,<br/> this create-near-app project runs in development mode.<br/>
-      It connects to <b><a href='https://wallet.testnet.near.org' alt='wallet.testnet.near.org'>wallet.testnet.near.org</a></b>.
-      This works just like the main network ("mainnet") wallet, but the NEAR Tokens on testnet aren't
-      convertible to other currencies – they're just for testing!
-    </p>
-    <p align='center' style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
-      To make use of this app running on the NEAR blockchain, you need to sign in!<br/> You can create a <a href="https://docs.near.org/docs/develop/basics/create-account">NEAR testnet account</a>, if you do not already have one. 
-    </p>
-    <p style={{ textAlign: 'center', marginTop: '1.5em', marginBottom: '1.5em', }}>
-      <button onClick={login}>Sign in</button>
-    </p>
-    <p style={{ fontSize: 'small', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '2px', padding: '4px', }}>
-        It is important to note that if you use this project from a gitpod, you will not need to configure your local development environment in any way to make use of this create-near-app based template.<br/>
-        You are, of course, free to clone the repository and work on it locally - however this may require you <br/>to perform additional configuration on your system.<br/><br/>
-        The documentation on the GitHub repository should be sufficient to get you going, and there are additional links to offsite documentation there as well. Enjoy, and happy hacking!<br/><br/>
-        - the Learn Team @ <a href="https://figment.io">Figment</a>
-    </p>
-    </main>
-    )
-  }
+        // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
+        <>
+            <Layout>
+                <Header style={{ background: 'rgba(0,0,0,25)', borderRadius: '4px', borderColor: 'rgb(255,255,0)', borderWidth: '2px' }}>
+                    <label style={{ fontSize: 'medium', background: 'rgba(0,1,1,15)', borderStyle: 'solid', borderRadius: '5px', borderWidth: '1px', padding: '16px', verticalAlign: 'middle', width: 'justify', textAlign: 'justify' }}>
+                        <Popover content={content} title="Account Balance">
+                            <Avatar size={48} icon={<UserOutlined />} /> <label htmlFor="greeting" style={{ color: 'var(--secondary)', borderBottom: '0px solid var(--secondary)' }}> {greeting} </label> {window.accountId}
+                        </Popover>
+                    </label>
+                    <Button type='primary' style={{ float: 'right', borderStyle: 'outset', borderWidth: '2px', fontSize: 'small', marginTop: '1.5em', marginBottom: '1.5em', textTransform: 'uppercase' }} onClick={logout}>
+                        Sign Out
+                    </Button>
+                </Header>
+                <Layout>
+                    <Content>
+                        <main>
+                            <Carousel autoplay>
+                                <div>
+                                    <h3 style={contentStyle}><img src={logowhite} size={30} style={{ width: '40px', }} /><p>dope</p></h3>
+                                </div>
+                                <div>
+                                    <h3 style={contentStyle}><img src={logowhite} size={30} style={{ width: '40px', }} /></h3>
+                                </div>
+                                <div>
+                                    <h3 style={contentStyle}><img src={logowhite} size={30} style={{ width: '40px', }} /></h3>
+                                </div>
+                                <div>
+                                    <h3 style={contentStyle}><img src={logowhite} size={30} style={{ width: '40px', }} /></h3>
+                                </div>
+                            </Carousel>
 
-  return (
-    // use React Fragment, <>, to avoid wrapping elements in unnecessary divs
-    <>
-      <button className="link" style={{ float: 'right' }} onClick={logout}>
-        Sign out
-      </button>
-      <main>
-        <h1>
-          <label
-            htmlFor="greeting"
-            style={{
-              color: 'var(--secondary)',
-              borderBottom: '2px solid var(--secondary)'
-            }}
-          >
-            {greeting}
-          </label>
-          {' '/* React trims whitespace around tags; insert literal space character when needed */}
-          {window.accountId}!
-        </h1>
-        <form onSubmit={async event => {
-          event.preventDefault()
+                        </main>
 
-          // get elements from the form using their id attribute
-          const { fieldset, greeting } = event.target.elements
+                        {showNotification && <Notification />}
 
-          // hold onto new user-entered value from React's SynthenticEvent for use after `await` call
-          const newGreeting = greeting.value
+                    </Content>
 
-          // disable the form while the value gets updated on-chain
-          fieldset.disabled = true
+                </Layout>
 
-          try {
-            // make an update call to the smart contract
-            await window.contract.setGreeting({
-              // pass the value that the user entered in the greeting field
-              message: newGreeting
-            })
-          } catch (e) {
-            alert(
-              'Something went wrong! ' +
-              'Maybe you need to sign out and back in? ' +
-              'Check your browser console for more info.'
-            )
-            throw e
-          } finally {
-            // re-enable the form, whether the call succeeded or failed
-            fieldset.disabled = false
-          }
+                <Footer>
+                    <hr />
+                    <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/roles/developer/contracts/intro">smart contract</a>
+                </Footer>
 
-          // update local `greeting` variable to match persisted value
-          setGreeting(newGreeting)
+            </Layout>
 
-          // show Notification
-          setShowNotification(true)
-
-          // remove Notification again after css animation completes
-          // this allows it to be shown again next time the form is submitted
-          setTimeout(() => {
-            setShowNotification(false)
-          }, 11000)
-        }}>
-          <fieldset id="fieldset">
-            <label
-              htmlFor="greeting"
-              style={{
-                display: 'block',
-                color: 'var(--gray)',
-                marginBottom: '0.5em'
-              }}
-            >
-              Change greeting
-            </label>
-            <div style={{ display: 'flex' }}>
-              <input
-                autoComplete="off"
-                defaultValue={greeting}
-                id="greeting"
-                onChange={e => setButtonDisabled(e.target.value === greeting)}
-                style={{ flex: 1 }}
-              />
-              <button
-                disabled={buttonDisabled}
-                style={{ borderRadius: '0 5px 5px 0' }}
-              >
-                Save
-              </button>
-            </div>
-          </fieldset>
-        </form>
-
-        <p>
-          Look at that! A Hello World app! This greeting is stored on the NEAR blockchain. Check it out:
-        </p>
-        <ol>
-          <li>
-            Look in <code>src/App.js</code> and <code>src/utils.js</code> – you'll see <code>getGreeting</code> and <code>setGreeting</code> being called on <code>contract</code>. What's this?
-          </li>
-          <li>
-            Ultimately, this <code>contract</code> code is defined in <code>assembly/main.ts</code> – this is the source code for your <a target="_blank" rel="noreferrer" href="https://docs.near.org/docs/roles/developer/contracts/intro">smart contract</a>.</li>
-          <li>
-            When you run <code>yarn dev</code>, the code in <code>assembly/main.ts</code> gets deployed to the NEAR testnet. You can see how this happens by looking in <code>package.json</code> at the <code>scripts</code> section to find the <code>dev</code> command.</li>
-        </ol>
-        <hr />
-        <p>
-          To keep learning, check out <a target="_blank" rel="noreferrer" href="https://docs.near.org">the NEAR docs</a> or look through some <a target="_blank" rel="noreferrer" href="https://examples.near.org">example apps</a>.
-        </p>
-
-      </main>
-
-      {showNotification && <Notification />}
-      
-    </>
-  )
+        </>
+    );
 }
 
 // this component gets rendered by App after the form is submitted
 function Notification() {
-  const urlPrefix = `https://explorer.${networkId}.near.org/accounts`
-  return (
-    <aside>
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
-        {window.accountId}
-      </a>
-      {' '/* React trims whitespace around tags; insert literal space character when needed */}
-      called method: 'setGreeting' in contract:
-      {' '}
-      <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
-        {window.contract.contractId}
-      </a>
-      <footer>
-        <div>✔ Succeeded</div>
-        <div>Just now</div>
-      </footer>
-    </aside>
-  )
+    const urlPrefix = `https://explorer.${networkId}.near.org/accounts`;
+    return (
+        <aside>
+            <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.accountId}`}>
+                {window.accountId}
+            </a>
+            {' '/* React trims whitespace around tags; insert literal space character when needed */}
+            called method: 'setGreeting' in contract:
+            {' '}
+            <a target="_blank" rel="noreferrer" href={`${urlPrefix}/${window.contract.contractId}`}>
+                {window.contract.contractId}
+            </a>
+            <footer>
+                <div>✔ Succeeded</div>
+                <div>Just now</div>
+            </footer>
+        </aside>
+    );
 }
--- src/config.js
+++ src/config.js
@@ -1,63 +1,58 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME || 'nibble'
+const CONTRACT_NAME = process.env.CONTRACT_NAME || 'nibble';
 
-function getConfig(env) {
-  switch (env) {
 
-  case 'production':
-  case 'mainnet':
-    return {
-      networkId: 'mainnet',
-      nodeUrl: 'https://rpc.mainnet.near.org',
-      contractName: CONTRACT_NAME,
-      walletUrl: 'https://wallet.near.org',
-      helperUrl: 'https://helper.mainnet.near.org',
-      explorerUrl: 'https://explorer.mainnet.near.org',
-    }
-  case 'development':
-  case 'testnet':
-    return {
-      networkId: 'testnet',
-      nodeUrl: 'https://rpc.testnet.near.org',
-      contractName: CONTRACT_NAME,
-      walletUrl: 'https://wallet.testnet.near.org',
-      helperUrl: 'https://helper.testnet.near.org',
-      explorerUrl: 'https://explorer.testnet.near.org',
-    }
-  case 'betanet':
-    return {
-      networkId: 'betanet',
-      nodeUrl: 'https://rpc.betanet.near.org',
-      contractName: CONTRACT_NAME,
-      walletUrl: 'https://wallet.betanet.near.org',
-      helperUrl: 'https://helper.betanet.near.org',
-      explorerUrl: 'https://explorer.betanet.near.org',
-    }
-  case 'local':
-    return {
-      networkId: 'local',
-      nodeUrl: 'http://localhost:3030',
-      keyPath: `${process.env.HOME}/.near/validator_key.json`,
-      walletUrl: 'http://localhost:4000/wallet',
-      contractName: CONTRACT_NAME,
-    }
-  case 'test':
-  case 'ci':
-    return {
-      networkId: 'shared-test',
-      nodeUrl: 'https://rpc.ci-testnet.near.org',
-      contractName: CONTRACT_NAME,
-      masterAccount: 'test.near',
-    }
-  case 'ci-betanet':
-    return {
-      networkId: 'shared-test-staging',
-      nodeUrl: 'https://rpc.ci-betanet.near.org',
-      contractName: CONTRACT_NAME,
-      masterAccount: 'test.near',
-    }
-  default:
-    throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`)
-  }
-}
+module.exports = function getConfig() {
+    let config = {
+        networkId: 'testnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org',
+        contractName: 'nibble',
+    };
 
-module.exports = getConfig
+    if (process.env.REACT_APP_ENV !== undefined) {
+        config = {
+            ...config,
+            GAS: '200000000000000',
+            DEFAULT_NEW_ACCOUNT_AMOUNT: '5',
+            GUESTS_ACCOUNT_SECRET: '',
+            contractMethods: {
+                changeMethods: [
+                    'new',
+                    'nft_mint',
+                    'nft_transfer',
+                    'add_guest',
+                    'remove_guest',
+                    'nft_approve_account_id',
+                    'nft_mint_guest',
+                    'nft_add_sale_guest',
+                    'nft_remove_sale_guest',
+                    'upgrade_guest'
+                ],
+                viewMethods: ['get_guest', 'get_token_ids', 'nft_token', 'get_sale'],
+            },
+            marketDeposit: '100000000000000000000000',
+            marketId: 'market.' + contractName
+        };
+    }
+
+    if (process.env.CONTRACT_NAME !== undefined) {
+        config = {
+            ...config,
+            contractName: CONTRACT_NAME
+        };
+    }
+
+    if (process.env.REACT_APP_ENV === 'production') {
+        config = {
+            ...config,
+            networkId: 'mainnet',
+            nodeUrl: 'https://rpc.mainnet.near.org',
+            walletUrl: 'https://wallet.near.org',
+            helperUrl: 'https://helper.mainnet.near.org',
+            contractName: 'near',
+        };
+    }
+
+    return config;
+};
